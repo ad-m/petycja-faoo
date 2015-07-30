94a334b9f7e80b1e8dd15c8db7a9e185fde51abd
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-07-30 14:53:09 +0200

    Add helper and locale for form

--- ankieta/petition_custom/forms.py
+++ ankieta/petition_custom/forms.py
@@ -1,6 +1,8 @@
 from petition.forms import BaseSignatureForm
-from crispy_forms.layout import Layout
+from crispy_forms.layout import Layout, Submit
 from crispy_forms.bootstrap import PrependedText
+from crispy_forms.helper import FormHelper
+from django.utils.translation import ugettext as _
 import swapper
 
 Signature = swapper.load_model("petition", "Signature")
@@ -9,6 +11,9 @@ Signature = swapper.load_model("petition", "Signature")
 class SignatureForm(BaseSignatureForm):
     def __init__(self, *args, **kwargs):
         super(SignatureForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', _('Sign'), css_class="btn-sign btn-lg btn-block"))
         self.helper.layout = Layout(
                 'first_name',
                 'last_name',
